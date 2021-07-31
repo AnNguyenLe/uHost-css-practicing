4f6147737f66e397e3c4f794599755b89e10c15e
an-nguyen-le <an@inspectorio.com>
2021-07-31 11:32:36 +0700

    using CSS transition, animation and CSS variable

--- shared.js
+++ shared.js
@@ -5,12 +5,14 @@ const modalNoButton = document.querySelector(".modal__action--negative");
 const selectPlanButtons = document.querySelectorAll(".plan button");
 const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
+const ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
 	selectPlanButtons[i].addEventListener("click", function () {
 		// backdrop.style.display = "block";
 		// modal.style.display = "block";
 		backdrop.classList.add("open");
+		backdrop.style.display = "block";
 		modal.classList.add("open");
 	});
 }
@@ -33,11 +35,28 @@ function closeModal() {
 	if (modal) {
 		modal.classList.remove("open");
 	}
+	backdrop.classList.remove("open");
+	setTimeout(function () {
+		backdrop.style.display = "none";
+	}, 200);
 }
 
 toggleButton.addEventListener("click", function () {
 	// mobileNav.style.display = "block";
 	// backdrop.style.display = "block";
 	mobileNav.classList.add("open");
-	backdrop.classList.add("open");
+	backdrop.style.display = "block";
+	setTimeout(function () {
+		backdrop.classList.add("open");
+	}, 10);
+});
+
+ctaButton.addEventListener("animationstart", function (event) {
+	console.log("Animation started", event);
+});
+ctaButton.addEventListener("animationend", function (event) {
+	console.log("Animation ended", event);
+});
+ctaButton.addEventListener("animationiteration", function (event) {
+	console.log("Animation iteration", event);
 });
